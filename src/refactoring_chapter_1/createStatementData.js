0e9f15427868e08d8fd64e976adb82792f417ae0
dodoongtak <themighty.tak@gmail.com>
2021-07-14 20:04:40 +0900

    refactor ♻️: split volume credit varaible
    Using one sigle source of credits as one variable brings the confusion
    to distinguish the each process of earning credits.
    
    See Also:
    - https://refactoring.com/catalog/splitVariable.html

--- src/refactoring_chapter_1/createStatementData.js
+++ src/refactoring_chapter_1/createStatementData.js
@@ -3,19 +3,22 @@ import { invoices, plays } from './fixtures';
 
 function accumulateVolumeCredit(invoice, plays) {
   // 포인트
-  let volumeCredits = 0;
+  let defaultCredits = 0;
+  let bonusCredits = 0;
 
   for (const perf of invoice.performances) {
     const play = plays[perf.playID];
 
     // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    if (perf.audience > 30) {
+      defaultCredits += perf.audience - 30;
+    }
 
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
+    if (play.type === 'comedy') bonusCredits += Math.floor(perf.audience / 5);
   }
 
-  return volumeCredits;
+  return defaultCredits + bonusCredits;
 }
 
 export default function statement(invoice, plays) {
